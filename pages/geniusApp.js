91469d1107d6005825126ad78c9fa6b275248267
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-03-19 12:29:51 -0400

    bullet points
    added button to genius page and removed bullets(Hopefully)

--- pages/geniusApp.js
+++ pages/geniusApp.js
@@ -6,6 +6,11 @@ import Layout from '@components/Layout'
 export default function geniusApp() {
   return (
     <Layout>
+        
+        <Link href="/index">
+          <a className="tab">Home</a>
+        </Link>
+
       <h1>placeholder 2</h1>
       <p>this is the second placeholder</p>
     </Layout>
