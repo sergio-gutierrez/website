9e681af272562b3f22f115380ecd2d5c6a5d7570
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-03-19 12:32:47 -0400

    Update geniusApp.js
    fixed buttons; layout was missing

--- pages/geniusApp.js
+++ pages/geniusApp.js
@@ -6,11 +6,15 @@ import Layout from '@components/Layout'
 export default function geniusApp() {
   return (
     <Layout>
-        
+        <div className="tabs">
         <Link href="/index">
           <a className="tab">Home</a>
         </Link>
+        <Link href="/geniusApp">
+          <a className="tab">App</a>
+        </Link>
 
+      </div>
       <h1>placeholder 2</h1>
       <p>this is the second placeholder</p>
     </Layout>
