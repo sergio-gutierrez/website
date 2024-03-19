f7b23a0f61cd9a7ff955d3db2cd661e4b656a8cc
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-03-19 11:44:09 -0400

    added page
    for genius app

--- pages/geniusApp.js
+++ pages/geniusApp.js
@@ -1,27 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>sergio-g</title>
+        <title>genius app goes here</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="placeholder title" />
         <p className="description">
-          maybe this will work? <code>pages/index.js</code>
+          tbh idk what im doing 
+          <code>pages/index.js</code>
         </p>
-
-        <Link href="/geniusApp">
-          <a>Go to Second Page</a>
-        </Link>
-
-
       </main>
 
       <Footer />
