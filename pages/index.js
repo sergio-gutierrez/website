21ea8d070c23110f2e248e33d64192c6cd8964e0
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-03-19 11:44:43 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>sergio-g</title>
+        <title>test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
