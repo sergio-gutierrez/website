00c03d8031af5e53dfb7ecf186976e8ccdd83f5c
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-03-21 18:48:57 -0400

    Update geniusApp.js
    import Link

--- pages/geniusApp.js
+++ pages/geniusApp.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Layout from '@components/Layout'
+import Link from 'next/link'
 
 export default function geniusApp() {
   return (
