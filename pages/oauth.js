c8174e0008d68c566ced4b51a988ffd19d438eb7
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-04-01 12:16:43 -0400

    Update oauth.js

--- pages/oauth.js
+++ pages/oauth.js
@@ -1,9 +1,11 @@
 //import {create} from 'simple-oauth2';
-const { AuthorizationCode } = require('simple-oauth2');
 //import { createOAuth } from 'simple-oauth2';
+const { AuthorizationCode } = require('simple-oauth2');
+
+const { OAuth2 } = require('simple-oauth2');
 
 
-const credentials ={
+const credentials = {
     client: {
         id: 'f0Z-QAgvyVYSdC18PYyAIE4xI8LmTVPCz5onapsYCDw-SPCq-9RMBlquN0t2BOgm',
         secret: '36Eqv9lOvk7I-t7HaggoZFHKVhafTsjEObQtXxA3QuIM6jp_W5NHvOwCd4a-ysFX2vWhaFVg_uLJmm9T37K1aA',
