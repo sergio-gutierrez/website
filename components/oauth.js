60c50b9cf7eaf5b8d34ae6f1719a559bb1307a04
sergio-gutierrez <46167228+sergio-gutierrez@users.noreply.github.com>
2024-04-18 19:13:35 -0400

    changed oauth
    lots of stuff

--- components/oauth.js
+++ components/oauth.js
@@ -1,9 +1,4 @@
-//import {create} from 'simple-oauth2';
-//import { createOAuth } from 'simple-oauth2';
-const { AuthorizationCode } = require('simple-oauth2');
-
-const { OAuth2 } = require('simple-oauth2');
-
+const { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } = require('simple-oauth2');
 
 const credentials = {
     client: {
@@ -11,14 +6,40 @@ const credentials = {
         secret: '36Eqv9lOvk7I-t7HaggoZFHKVhafTsjEObQtXxA3QuIM6jp_W5NHvOwCd4a-ysFX2vWhaFVg_uLJmm9T37K1aA',
     },
     auth: {
-        tokenHost: 'https://api.genius.com/oauth',
+        tokenHost: 'https://api.genius.com',
         tokenPath: '/oauth/token',
         authorizePath: '/oauth/authorize',
-        //response_type: 'code',
-        grant_type: "authorization_code",
+        //response_type: 'token',
+        //grant_type: 'authorization_code',
     },
 };
 
 const oauth2 = new AuthorizationCode(credentials);
 
 export default oauth2;
+
+export const handleOAuthCallback = async (code) => {
+    try{
+        if(code){
+            const tokenParams = {
+                code,
+                client_id: 'f0Z-QAgvyVYSdC18PYyAIE4xI8LmTVPCz5onapsYCDw-SPCq-9RMBlquN0t2BOgm',
+                client_secret: '36Eqv9lOvk7I-t7HaggoZFHKVhafTsjEObQtXxA3QuIM6jp_W5NHvOwCd4a-ysFX2vWhaFVg_uLJmm9T37K1aA',
+                redirect_uri: 'http://sergio-g.com/geniusApp',
+                response_type: 'code',
+                grant_type: 'authorization_code',
+            };
+            const tokenResponse = await oauth2.authorizationCode.getTokens(tokenParams);
+            console.log('Token Response', tokenResponse);
+
+            const token = oauth2.accessToken.create(tokenResponse);
+            return token.token.access_token;
+        }else{
+            console.error('Authorization code not found. (Oauth) ');
+            return null;
+        }
+    }catch (error){
+        console.error('Error exchanging authorization code for access token.', error);
+        return null;
+    }
+};
--- pages/geniusApp.js
+++ pages/geniusApp.js
@@ -2,7 +2,7 @@ import Layout from '@components/Layout';
 import Link from 'next/link';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import oauth2 from '../components/oauth';
+import { handleOAuthCallback } from '@components/oauth';
 
 //defines default function, geniusApp
 export default function geniusApp() {
@@ -12,37 +12,23 @@ export default function geniusApp() {
   const [accessToken, setAccessToken] = useState('');  
 
   useEffect(() => {
-    const handleOAuthCallback = async () => {
+    const handleOAuth = async () => {
       try {
         const params = new URLSearchParams(window.location.search); // extracts URL params from the current URL
         const code = params.get('code'); // gets the val of the 'code' param form the URL query string
-        console.log('Authorization Code', code); // log for authorization code 
+        console.error('Authorization Code (Genius App Page)', code); // log for authorization code 
 
-        if(code){ // checks if the auth code is present in the URL and does token exchange if it exists
-          const tokenParams = {
-            code,
-            client_id: 'f0Z-QAgvyVYSdC18PYyAIE4xI8LmTVPCz5onapsYCDw-SPCq-9RMBlquN0t2BOgm',
-            client_secret: '36Eqv9lOvk7I-t7HaggoZFHKVhafTsjEObQtXxA3QuIM6jp_W5NHvOwCd4a-ysFX2vWhaFVg_uLJmm9T37K1aA',
-            redirect_uri: 'http://sergio-g.com/geniusApp',
-            response_type: 'code',
-            grant_type: 'authorization_code',
-          };
-          // CHECK THIS LINE OF CODE
-          const tokenResponse = await oauth2.authorizationCode.getToken(tokenParams); // calls getToken, passes tokenParams as arg.
-          console.log('Token Response', tokenResponse);
-
-          const token = oauth2.accessToken.create(tokenResponse); //calls create method, passing tokenResponse, create token object
-          setAccessToken(token.token.access_token); //sets accessToken to value of token.token.acess_token
-        } else {
-          console.error('Authorization code not found in URL');
+        const accessToken = await handleOAuthCallback(code);
+        if (accessToken) {
+          setAccessToken(accessToken);
         }
-      }catch (error) {
-        console.error('Error exchanging authorization code for access token.', error);
+      }catch (error){
+        console.error('Error handling OAuth callback', error);
       }
-      };
-      handleOAuthCallback();
-      }, []);
-    //console.log('Submitted Song Name', songName);
+    };
+    handleOAuth();
+  }, 
+  []);
 
     const handleSearch = async (event) => {
       event.preventDefault();
@@ -55,7 +41,7 @@ export default function geniusApp() {
     
       const firstSong = response.data.response.hits[0];
       if(firstSong){
-        const songId = firstSong.result.id;
+        const songId = firstSong.result.id; 
         const lyricsResponse = await axios.get('https://api.genius.com/songs/${songId}/lyrics', {
           headers:{
             Authorization: `Bearer -nX8pzNozRk98MybemJdPOPo9EnheAptUvp5-CBLnGWoCXc8eL4WJfqWKiSaypod`,
